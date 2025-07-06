70d2f693fc2be257335eb80e72c4f131429d5f55
Faruk Emre Gokmen <emreemre200412@gmail.com>
2025-07-06 17:55:08 +0300

    Project created.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KutuphaneYonetimSistemi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
